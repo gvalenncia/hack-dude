84c4bb068302d08e3d72b58f0072493e34cb8cb7
german valencia <gvalenncia@gmail.com>
2018-11-14 17:34:20 -0500

    Implementing a simple web task

--- index.js
+++ index.js
@@ -1,16 +1,50 @@
 'use latest';
 
-import express from 'express';
-import { fromExpress } from 'webtask-tools';
-import bodyParser from 'body-parser';
+import axios from 'axios';
 
-const app = express();
+const DANISH = 'danish';
+const SCOTISH = 'scotish';
+const SUPERLIGA_ID = '/271';
+const PREMIERSHIP_ID = '/501';
 
-app.set("view engine", "ejs");
-app.use(bodyParser.json());
+module.exports = function (ctx, done) {
+  let league = getLeagueIdFromName(ctx.data.name);
 
-app.get('/', (req, res) => {
-  res.render("index", {});
-});
+  if(league){
+    let url = '	https://soccer.sportmonks.com/api/v2.0/leagues'+ league +
+    '?api_token=PMOGRNpIH2oxAANr6EBmKycVeMyzq41uGhOaVjCTvmUrVOnxa2bt3ympnxoH';
+  
+    axios.get(url)
+    .then(response => {
+      done(null, 'Hello, ' + ctx.data.name);
+    })
+  } else {
+    done(null, 'Provide a valid league name.');
+  }
+}
 
-module.exports = fromExpress(app);
+function getLeagueIdFromName(name){
+  if(name === DANISH) {
+    return SUPERLIGA_ID;
+  } else if (name === SCOTISH){
+    return PREMIERSHIP_ID;
+  } else {
+    return undefined;
+  }
+}
+
+function renderView(content) {
+  return `
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <meta charset="utf-8">
+      <title>${content.title}</title>
+    </head>
+
+    <body>
+      ${content.body}
+    </body>
+    </html>
+  `;
+}
