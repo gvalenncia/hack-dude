99a5fd4944f5409f8f93d98cb63a14ce88d4145c
german valencia <gvalenncia@gmail.com>
2018-11-21 10:22:50 -0500

    Fetching players

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use latest';
 
 import axios from 'axios';
+import json2html from 'node-json2html';
 
 const DANISH = 'danish';
 const SCOTISH = 'scotish';
@@ -9,17 +10,18 @@ const PREMIERSHIP_ID = '/501';
 const API_TOKEN = 'PMOGRNpIH2oxAANr6EBmKycVeMyzq41uGhOaVjCTvmUrVOnxa2bt3ympnxoH';
 module.exports = function (ctx, req, res) {
   let league = getLeagueIdFromName(ctx.data.name);
-
   if(league){
     axios.get('https://soccer.sportmonks.com/api/v2.0/leagues'+ league,
-    {params:{ 'api_token': API_TOKEN }})
+    {params: { 'api_token': API_TOKEN }})
     .then(leagues => {
       let sessionId = leagues.data.data.current_season_id;
       return axios.get('https://soccer.sportmonks.com/api/v2.0/topscorers/season/' + sessionId + '/aggregated',
-      {params:{ 'api_token': API_TOKEN }});
+      {params: { 'api_token': API_TOKEN }});
     }).then(topscorers => {
-      res.writeHead(200, { 'Content-Type': 'text/html'});
-      res.end('<h1>hello baby</h1>');
+      return axios.all(getPlayerPromises(topscorers.data.data.aggregatedGoalscorers.data));
+    }).then( resolves => {
+      res.writeHead(200, { 'Content-Type': 'application/json'});
+      res.end('');
     }).catch(error => {
       res.writeHead(500, { 'Content-Type': 'application/json'});
       res.end('{"message": "Unexpected Error, try again later."}');
@@ -30,14 +32,22 @@ module.exports = function (ctx, req, res) {
   }
 }
 
+function getPlayerPromises (players) {
+  let promises = [];
+    players.forEach(player => {
+      let promise = axios.get('https://soccer.sportmonks.com/api/v2.0/players/' + player.player_id,
+      {params: {'api_token': API_TOKEN}}).then(response => {return response.data;});
+      promises.push(promise);
+    });
+    return promises;
+}
 function getLeagueIdFromName(name){
   if(name === DANISH) {
     return SUPERLIGA_ID;
   } else if (name === SCOTISH){
     return PREMIERSHIP_ID;
-  } else {
-    return undefined;
   }
+  return undefined;
 }
 
 function renderView(content) {
